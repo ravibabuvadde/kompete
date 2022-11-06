ae1dfdc5138b429687e048e0f01b1533527e3d66
ravibabuvadde <79750913+ravibabuvadde@users.noreply.github.com>
2022-11-06 23:28:37 +0530

    fetch() replaced with axios

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
 
 app.set('view-engine','ejs')
 
 app.use(express.static('static'))
 
-app.get('/',(req,res)=>{
-    fetch('https://kontests.net/api/v1/all')
-    .then(res => res.json())
+app.get('/',async(req,res)=>{
+    await axios.get('https://kontests.net/api/v1/all')
     .then(data => {
-        res.render('index.ejs',{contests:data})
+        res.render('index.ejs',{contests:data.data})
     })
     
     
